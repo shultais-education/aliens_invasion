822cdfd9dd8a419dbc79112e8ec56311b9e29d20
Nikita Shultais <nikita@shultais.ru>
2021-01-12 19:57:54 +0200

    Обработка событий. Картирки и тд.

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """
+    Класс для хранения всех настроек игры.
+    """
+
+    def __init__(self):
+        self.screen_width = 1920
+        self.screen_height = 928
+        self.bg_color = (230, 230, 230)
---
+++ ship.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+class Ship:
+    """
+    Класс управления кораблем.
+    """
+
+    def __init__(self, ai_game):
+        """
+        Инициализирует корабль и задает его начальную позицию.
+        """
+
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Загружает изображение корабля и получает прямоугольник.
+        self.image = pygame.image.load("images/ship.png")
+        self.rect = self.image.get_rect()
+
+        # Каждый новый корабль появляется у нижнегоа края экрана.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """
+        Ресует корабль в текущей позиции
+        """
+        self.screen.blit(self.image, self.rect)
