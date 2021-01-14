2b434f8df286c62c9a15e86dd9df398c68e39579
Nikita Shultais <nikita@shultais.ru>
2021-01-14 20:18:53 +0200

    Ограничение снарядов и небольшой рефакторинг.

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,7 +32,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -69,6 +69,14 @@ class AlienInvasion:
         elif event.key in (pygame.K_q, pygame.K_ESCAPE):
             sys.exit()
 
+    def _update_bullets(self):
+        self.bullets.update()
+
+        # Удаление снарядов, вышедших за край экрана.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         """
         Обновляет изображения на экране и отображает новый экран.
@@ -85,8 +93,9 @@ class AlienInvasion:
         """
         Создание нового снаряда и включение его в группу bullets.
         """
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
 if __name__ == '__main__':
