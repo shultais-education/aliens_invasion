3fd6132b1e7940ada34d971deea58247bcf267b3
Nikita Shultais <nikita@shultais.ru>
2021-01-30 20:18:31 +0200

    Уменьшил высоту флота

--- alien_invasion.py
+++ alien_invasion.py
@@ -207,7 +207,7 @@ class AlienInvasion:
 
         # Определяем количество рядов, которые помещаются на экране
         ship_height = self.ship.rect.height
-        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        available_space_y = (self.settings.screen_height - (5 * alien_height) - ship_height)
         number_rows = available_space_y // (2 * alien_height)
 
         # Создание флота вторжения
@@ -223,7 +223,7 @@ class AlienInvasion:
         alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
-        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        alien.rect.y = 4 * alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _create_bang(self, alien):
